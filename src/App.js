a90f6fa9e1aca460e33584c4bd9f833a1ef0a0ab
Sophie Krag <sophie.9@live.nl>
2021-08-07 10:29:19 +0200

    Create login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
 const App = () => {
   return (
     <>
       <p>Hello</p>
       <Signup/>
+      <Login/>
     </>
   );
 };
