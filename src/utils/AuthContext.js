a8d1fe42b6d75ac4b669a63ab559cc68da84f466
Sophie Krag <sophie.9@live.nl>
2021-08-22 18:43:07 +0200

    Create private route

--- src/utils/AuthContext.js
+++ src/utils/AuthContext.js
@@ -1,12 +1,22 @@
 import React, { useState, createContext } from "react";
+import axiosApi from "./AxiosApi";
 
 export const IsAuthContext = createContext({});
 
 const AuthContext = ({ children }) => {
-  const [isAuth, setIsAuth] = useState(true);
+  const [isAuth, setIsAuth] = useState(false);
+
+  const isLoggedIn = async () => {
+    try {
+      const result = await axiosApi.get("checkAuth");
+      setIsAuth(result.status === 200);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <IsAuthContext.Provider value={isAuth}>
+    <IsAuthContext.Provider value={{ isAuth, isLoggedIn }}>
       {children}
     </IsAuthContext.Provider>
   );
--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -1,15 +1,30 @@
 import React, { useContext } from "react";
-import { Redirect, Route } from "react-router-dom";
+import { Redirect, Route, Link } from "react-router-dom";
 
 import { IsAuthContext } from "./AuthContext";
 
 function PrivateRoute({ children, ...restOfProps }) {
-  const isAuth = true
-  
-  return isAuth ? 
-    (<Route {...restOfProps}>{children}</Route>)
-       :
-    (<Redirect exact to="/" /> )
+  const { isLoggedIn } = useContext(IsAuthContext);
+  const { isAuth } = useContext(IsAuthContext);
+
+  isLoggedIn();
+
+  if (!isAuth) {
+    return (
+      <p>
+        You need to be logged in. Please login{" "}
+        <Link exact to="/">
+          here
+        </Link>
+      </p>
+    );
+  }
+
+  return isAuth ? (
+    <Route {...restOfProps}>{children}</Route>
+  ) : (
+    <Redirect exact to="/" />
+  );
 }
 
 export default PrivateRoute;
