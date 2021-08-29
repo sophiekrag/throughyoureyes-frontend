f6efe9f20fefa5d2158c89e7f85c656d3d2399fc
Sophie Krag <sophie.9@live.nl>
2021-08-29 11:35:40 +0200

    Changed url

--- src/utils/AxiosApi.js
+++ src/utils/AxiosApi.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosApi = axios.create({
-  //baseURL: "https://through-your-eyes.herokuapp.com/api/",
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://through-your-eyes.herokuapp.com/api/",
+  //baseURL: "http://localhost:5000/api",
   timeout: 10000,
   withCredentials: true,
 });
